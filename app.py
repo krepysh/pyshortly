5ba45bbf6f434fad5cb40539be9ca89edb00a1c2
Anton Topchii <anton.topchii@getbyrd.com>
2023-05-15 19:29:53 +0200

    Added add url form

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, url_for, redirect, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return redirect(url_for("create_url"))
+
+
+@app.route("/url/new", methods=["GET", "POST"])
+def create_url():
+    if request.method == "POST":
+        url = request.form["url"]  # noqa
+    return render_template("new_url.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
