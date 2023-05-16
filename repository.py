bcb071046586ace963119c88fe9076bedc582873
Anton Topchii <anton.topchii@getbyrd.com>
2023-05-16 17:35:46 +0200

    Add method overloading to repositories

--- repository.py
+++ repository.py
@@ -1,6 +1,7 @@
+from abc import abstractmethod, ABC
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Optional, Dict, Union, Iterable
+from typing import Optional, Dict, Union, Iterable, overload
 
 
 @dataclass
@@ -10,13 +11,35 @@ class Link:
     created_at: datetime
 
 
-class LinkRepository:
-    pass
+class LinkRepository(ABC):
+    @overload
+    def get(self) -> Iterable[Link]:
+        ...
+
+    @overload
+    def get(self, hash_id: str) -> Link:
+        ...
+
+    @abstractmethod
+    def get(self, hash_id: Optional[str] = None) -> Union[Iterable[Link], Link]:
+        pass
+
+    @abstractmethod
+    def create(self, link: Link) -> Link:
+        pass
 
 
 class InMemoryLinkRepository(LinkRepository):
-    def __init__(self):
+    def __init__(self) -> None:
         self._links: Dict[str, Link] = {}
+
+    @overload
+    def get(self) -> Iterable[Link]:
+        ...
+
+    @overload
+    def get(self, hash_id: str) -> Link:
+        ...
 
     def get(self, hash_id: Optional[str] = None) -> Union[Iterable[Link], Link]:
         if hash_id is None:
