fe538904bc6ad4c9eda162936c7a1974c0c466f4
Anton Topchii <anton.topchii@getbyrd.com>
2023-05-16 19:42:01 +0200

    Check url a bit

--- services.py
+++ services.py
@@ -1,10 +1,22 @@
+from urllib import parse
 from datetime import datetime
 
 from repository import Link, repository
 from werkzeug.security import generate_password_hash
 
 
+class InvalidUrl(Exception):
+    pass
+
+
+def validate_url(url):
+    schema, netloc, *_ = parse.urlparse(url)
+    if not schema or not netloc:
+        raise InvalidUrl
+
+
 def create_short_url(url: str) -> Link:
+    validate_url(url)
     new_hash = generate_password_hash(url, method="pbkdf2:sha256:6")[-6:]
     new_link = Link(url=url, hash_id=new_hash, created_at=datetime.utcnow())
     new_link = repository.create(new_link)
