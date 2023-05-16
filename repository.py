9edd98d0c55c203ae7e140da91d9f758ec95a141
Anton Topchii <anton.topchii@getbyrd.com>
2023-05-16 18:47:00 +0200

    Hello fs repository

--- repository.py
+++ repository.py
@@ -1,7 +1,11 @@
+import dataclasses
+import json
+import os
 from abc import abstractmethod, ABC
 from dataclasses import dataclass
 from datetime import datetime
-from typing import Optional, Dict, Union, Iterable, overload
+from pathlib import Path
+from typing import Optional, Dict, Union, overload, List
 
 
 @dataclass
@@ -13,7 +17,7 @@ class Link:
 
 class LinkRepository(ABC):
     @overload
-    def get(self) -> Iterable[Link]:
+    def get(self) -> List[Link]:
         ...
 
     @overload
@@ -21,7 +25,7 @@ class LinkRepository(ABC):
         ...
 
     @abstractmethod
-    def get(self, hash_id: Optional[str] = None) -> Union[Iterable[Link], Link]:
+    def get(self, hash_id: Optional[str] = None) -> Union[List[Link], Link]:
         pass
 
     @abstractmethod
@@ -34,14 +38,14 @@ class InMemoryLinkRepository(LinkRepository):
         self._links: Dict[str, Link] = {}
 
     @overload
-    def get(self) -> Iterable[Link]:
+    def get(self) -> List[Link]:
         ...
 
     @overload
     def get(self, hash_id: str) -> Link:
         ...
 
-    def get(self, hash_id: Optional[str] = None) -> Union[Iterable[Link], Link]:
+    def get(self, hash_id: Optional[str] = None) -> Union[List[Link], Link]:
         if hash_id is None:
             return list(self._links.values())
         else:
@@ -49,6 +53,47 @@ class InMemoryLinkRepository(LinkRepository):
 
     def create(self, link: Link) -> Link:
         self._links[link.hash_id] = link
+        return link
+
+
+class FileSystemLinkRepository(LinkRepository):
+    def __init__(self, path: Union[str, Path]):
+        self.path = os.path.join(path, "db")
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+
+    def create(self, link: Link) -> Link:
+        with open(os.path.join(self.path, f"{link.hash_id}.txt"), "x") as f:
+            json.dump(dataclasses.asdict(link), fp=f)
+        return link
+
+    @overload
+    def get(self) -> List[Link]:
+        ...
+
+    @overload
+    def get(self, hash_id: str) -> Link:
+        ...
+
+    def get(self, hash_id: Optional[str] = None) -> Union[Link, List[Link]]:
+        if hash_id is not None:
+            return self._read_single_link(hash_id)
+        else:
+            links = []
+            for filename in os.listdir(self.path):
+                hash_id = filename.removesuffix(".txt")
+                link = self._read_single_link(hash_id)
+                links.append(link)
+            return links
+
+    def _read_single_link(self, hash_id):
+        with open(os.path.join(self.path, f"{hash_id}.txt")) as f:
+            link_json = json.load(f)
+            link = Link(
+                url=link_json["url"],
+                hash_id=link_json["hash_id"],
+                created_at=link_json["created_at"],
+            )
         return link
 
 
--- test_repository.py
+++ test_repository.py
@@ -2,35 +2,50 @@ from datetime import datetime
 
 import pytest
 
-from repository import Link, InMemoryLinkRepository
+from repository import Link, InMemoryLinkRepository, FileSystemLinkRepository
 
 
 @pytest.fixture
-def repository():
+def repository_in_mem():
     return InMemoryLinkRepository()
 
 
-def test_repository_is_empty(repository):
-    assert repository.get() == []
+@pytest.fixture
+def fs_repository(tmp_path):
+    return FileSystemLinkRepository(tmp_path)
 
 
-def test_create(repository):
+def test_repository_is_empty(repository_in_mem):
+    assert repository_in_mem.get() == []
+
+
+def test_create(repository_in_mem):
     link = Link(url="test_url", hash_id="abc", created_at=datetime.utcnow())
-    created_link = repository.create(link)
+    created_link = repository_in_mem.create(link)
     assert link == created_link
 
 
-def test_get_single_link(repository):
+def test_get_single_link(repository_in_mem):
     link = Link(url="test_url", hash_id="abc", created_at=datetime.utcnow())
-    created_link = repository.create(link)
-    retrieved_link = repository.get(link.hash_id)
+    created_link = repository_in_mem.create(link)
+    retrieved_link = repository_in_mem.get(link.hash_id)
 
     assert created_link == retrieved_link
 
 
-def test_get_not_exist_hash(repository):
+def test_get_not_exist_hash(repository_in_mem):
     assert True
 
 
-def test_create_duplicate_hash(repository):
+def test_create_duplicate_hash(repository_in_mem):
     assert True
+
+
+def test_create_in_fs(fs_repository):
+    fs_repository.create(Link(url="test_link", hash_id="hash", created_at=""))
+    link = fs_repository.get("hash")
+    links = fs_repository.get()
+
+    assert link.url == "test_link"
+    assert len(links) == 1
+    assert links[0].url == "test_link"
