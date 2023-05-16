d6cdee8c11699e046da7ff468eeb0bf0ac5efc68
Anton Topchii <anton.topchii@getbyrd.com>
2023-05-16 14:57:02 +0200

    Add list link page

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, url_for, redirect, render_template, request
+import os
 
+from flask import Flask, url_for, redirect, render_template, request, flash
+
+from repository import repository
 from services import create_short_url
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -14,8 +18,16 @@ def index():
 def create_url():
     if request.method == "POST":
         url = request.form["url"]
-        link = create_short_url(url=url)  # noqa
+        link = create_short_url(url=url)
+        flash(f"You have successfully created a shorten url for {link.url}")
+        return redirect(url_for("link_list"))
     return render_template("new_url.html")
+
+
+@app.route("/link/list")
+def link_list():
+    links = repository.get()
+    return render_template("link_list.html", links=links)
 
 
 if __name__ == "__main__":
