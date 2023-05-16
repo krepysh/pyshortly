68eb3518e61916a927b547a34d266ab2327bb6c9
Anton Topchii <anton.topchii@getbyrd.com>
2023-05-16 15:30:47 +0200

    Add: basic test for app

---
+++ test_app.py
@@ -0,0 +1,19 @@
+import pytest
+
+from app import app
+
+
+@pytest.fixture()
+def client():
+    app.config["SECRET_KEY"] = "test_secret"
+
+    yield app.test_client()
+
+
+def test_add_link(client):
+    url = "https://example.com/long"
+    result = client.post("/link/new", data=dict(url=url), follow_redirects=True)
+
+    assert result.status_code == 200
+    assert url.encode("utf-8") in result.data
+    assert result.request.path == "/link/list"
