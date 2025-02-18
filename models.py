be3fdedf37fcc7d1862b5a27ed2b22b8b9be8336
Anton Topchiy <topchiy.anton@gmail.com>
2025-02-18 08:43:35 +0100

    Intoduce url db model

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
@@ -27,3 +30,11 @@ class User(db.Model):
     @property
     def is_anonymous(self):
         return False
+
+
+class Url(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    url: Mapped[str] = mapped_column()
+    hash_id: Mapped[str] = mapped_column(index=True)
+    created_at: Mapped[datetime] = mapped_column(default=datetime.utcnow)
+    views: Mapped[int] = mapped_column(default=0)
